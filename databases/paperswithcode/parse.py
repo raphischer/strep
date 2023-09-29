54a1d912147012411eb0cbe4e87150c77dfdbc8f
Raphael Fischer <27725573+raphischer@users.noreply.github.com>
2023-09-29 16:12:57 +0200

    updated parsing

--- databases/paperswithcode/parse.py
+++ databases/paperswithcode/parse.py
@@ -1,5 +1,6 @@
 import os
 import math
+import json
 
 import numpy as np
 from paperswithcode import PapersWithCodeClient
@@ -15,15 +16,21 @@ def collect_evaluation_meta(client):
         results = client.evaluation_list(page=page + 1).results
         for res in results:
             evals_meta[res.id] = (res.dataset, res.task)
+        # if page > 3:
+        #     break
     return evals_meta
 
 
 def collect_eval_metrics(client, eval_id, eval_meta):
-    # fname = os.path.join(FDIR, 'results', f'{eval_id}.pkl')
     init_list = client.evaluation_result_list(evaluation_id=eval_id)
+    metrics = set(['methodology', 'paper', 'evaluated_on'])
+    for res in init_list.results:
+        metrics = metrics.union(set(list(res.metrics.keys())))
+    other_stats = {'n_results': init_list.count, 'n_metrics': len(metrics)}
+    df = None
+
     if init_list.count > 9:
         eval_metrics = []
-        
         n_pages = math.ceil(init_list.count / len(init_list.results))
         for page in range(n_pages):
             if page > 0: # first page already loaded
@@ -47,15 +54,26 @@ def collect_eval_metrics(client, eval_id, eval_meta):
             except Exception:
                 pass
         
-        if df.select_dtypes('number').columns.size > 2:
+        # filter for numeric cols with at least 20% of valid entries
+        num_cols = [col for col in df.select_dtypes('number').columns if df[col].dropna().size / df[col].size > 0.2]
+        df_num = df[num_cols]
+        # check if there are at least 3 metrics and 10 results
+        if df_num.columns.size > 2 and df_num.dropna(how='all').shape[0] > 9:
             df['dataset'] = eval_meta[0]
             df['task'] = eval_meta[1]
-            return df.astype(str)
+            if df_num.shape != df_num.dropna(how='all').shape:
+                df = df.loc[df_num.dropna(how='all').index]
+            df = df.rename(columns={col: col.lower().strip().replace('-', '_') for col in df})
+            df = df.reset_index(drop=True)
+        else:
+            df = None
+    return df, other_stats
 
 
 if __name__ == '__main__':
     FDIR = os.path.dirname(__file__)
-    MERGED = os.path.join(FDIR, 'database15.pkl')
+    MERGED = os.path.join(FDIR, 'database_new.pkl')
+    OTHER = os.path.join(FDIR, 'other_stats.json')
 
     client = PapersWithCodeClient()
 
@@ -63,12 +81,15 @@ if __name__ == '__main__':
         merged = pd.read_pickle(MERGED)
     else:
         evals_meta = collect_evaluation_meta(client)
-        evals_metrics = {}
+        evals_metrics, other_stats = {}, {}
         for eval_id, eval_meta in tqdm(evals_meta.items(), total=len(evals_meta), desc='assessing evaluation metrics'):
-            df = collect_eval_metrics(client, eval_id, eval_meta)
+            df, other = collect_eval_metrics(client, eval_id, eval_meta)
+            other_stats[eval_id] = other
             if df is not None:
                 evals_metrics[eval_id] = df
 
+        with open(OTHER, 'w') as jf:
+            json.dump(other_stats, jf)
         merged = pd.concat(list(evals_metrics.values())).reset_index(drop=True)
         merged = merged.rename(columns={'model': 'model_name'})
         merged['model'] = merged[['methodology', 'paper']].apply(lambda val: '{} from {}'.format(*val.astype(str)), axis=1, raw=True)
@@ -82,4 +103,3 @@ if __name__ == '__main__':
         data = data.dropna(how='all', axis=1)
         n_tasks, n_papers, m_methods = pd.unique(data['task']).size, pd.unique(data['paper']).size, pd.unique(data['methodology']).size
         print(f"{gr:<45} {n_tasks:<2} tasks {n_papers:<3} papers {m_methods:<3} methods {str(data.shape):<10} results")
-        # database['configuration'] = database.aggregate(lambda row: ' - '.join([row['task'], row['dataset'], row['model']]), axis=1)
