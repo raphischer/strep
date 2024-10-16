9aaec76b9cb4c23cf85fe63ef76f80ac86acaef5
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2024-10-16 15:33:46 +0000

    optimized code for batch size obtaining, starting big run now

--- experiments/imagenet/main.py
+++ experiments/imagenet/main.py
@@ -5,56 +5,46 @@ import mlflow
 import pandas as pd
 from codecarbon import OfflineEmissionsTracker
 
-def print_colored_block(message, ok=True):
-    col = '\033[92m' if ok else '\033[91m'
-    print(col + (u"\u2588"*60 + '\n')*4 + '\n')
-    print(f"{message}\n")
-    print((u"\u2588"*60 + '\n')*4 + '\033[0m')
-
-model_subset_sizes = {
-    'ConvNeXtBase': 100,
-    'ResNet152V2': 2,
-    'InceptionResNetV2': 4,
-    'EfficientNetB7': 10,
-    'EfficientNetB6': 8,
-    'EfficientNetB5': 6,
-    'EfficientNetB4': 3,
-    'DenseNet201': 2,
-    'ConvNeXtSmall': 2
-}
+from util import print_colored_block
+from batch_sizes import lookup_batch_size, find_ideal_batch_size
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Inference benchmarking with keras models on ImageNet")
     # data and model input
     parser.add_argument("--experiment", default="/home/fischer/repos/mlprops/experiments/imagenet/")
-    parser.add_argument("--model", default="EfficientNetB6")
+    parser.add_argument("--model", default="MobileNetV3Small")
     parser.add_argument("--dataset", default=None)
     parser.add_argument("--datadir", default="/data/d1/fischer_diss/imagenet")
-    parser.add_argument("--measure_power_secs", default=1)
-    parser.add_argument("--nogpu", default=0)
+    parser.add_argument("--measure_power_secs", default=0.5)
+    parser.add_argument("--nogpu", type=int, default=0)
     parser.add_argument("--subset", default=0)
     args = parser.parse_args()
-
     mlflow.log_dict(args.__dict__, 'config.json')
     
-    # init
+    # if required, disable gpu
     if args.nogpu:
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-    from util import load_data_and_model
-    model, ds, meta = load_data_and_model(args.datadir, args.model, nogpu=args.nogpu)
+
+    # identify batch_size and load data
+    batch_size = lookup_batch_size(args.model) or find_ideal_batch_size(args.model, args.nogpu, args.datadir)
+    from experiments.imagenet.data_and_model_loading import load_data_and_model, MODEL_SUBSET_SIZES # import inits tensorflow, so only import now
+    model, ds, meta = load_data_and_model(args.datadir, args.model, batch_size=batch_size)
     meta['dataset'] = 'ImageNet (ILSVRC 2012)'
     meta['task'] = 'Inference'
+    for key, val in meta.items():
+        mlflow.log_param(key, val)
     n_samples = 50000
 
     # take a snippet of the data, if only testing a subset (e.g., for energy profiling)
     if args.subset:
-        if args.nogpu: # on CPU, models are about 10x slower
-            ds = ds.take(len(ds) // 10)
-            n_samples = n_samples // 10
-        if args.model in model_subset_sizes: # take less data for big models
-            ds = ds.take(len(ds) // model_subset_sizes[args.model])
-            n_samples = n_samples // model_subset_sizes[args.model]
+        if args.nogpu: # on CPU, models are about 5x slower
+            ds = ds.take(len(ds) // 5)
+            n_samples = n_samples // 5
+        if args.model in MODEL_SUBSET_SIZES: # take less data for big models
+            ds = ds.take(len(ds) // MODEL_SUBSET_SIZES[args.model])
+            n_samples = n_samples // MODEL_SUBSET_SIZES[args.model]
+    mlflow.log_param('n_samples', n_samples)
 
     # evaluate on validation
     tracker = OfflineEmissionsTracker(measure_power_secs=args.measure_power_secs, log_level='warning', country_iso_code="DEU")
@@ -80,6 +70,7 @@ if __name__ == '__main__':
     # assess resource consumption
     emissions = 'emissions.csv'
     emission_data = pd.read_csv('emissions.csv').to_dict()
+    eval_res['time_total'] = emission_data['duration'][0]
     eval_res['running_time'] = emission_data['duration'][0] / n_samples
     eval_res['power_draw'] = emission_data['energy_consumed'][0] * 3.6e6 / n_samples
     eval_res['parameters'] = model.count_params()
@@ -87,8 +78,6 @@ if __name__ == '__main__':
     # log results
     for key, val in eval_res.items():
         mlflow.log_metric(key, val)
-    for key, val in meta.items():
-        mlflow.log_param(key, val)
     mlflow.log_artifact(emissions)
     
     # cleanup
--- experiments/imagenet/run_all.sh
+++ experiments/imagenet/run_all.sh
@@ -6,16 +6,13 @@ exp_create_str=$(mlflow experiments create -n $exp_name)
 echo $exp_create_str
 exp_id=$(echo $exp_create_str | awk '{print $NF}')
 
-for m in "ConvNeXtBase" "ConvNeXtLarge" "ConvNeXtSmall" "ConvNeXtTiny" "ConvNeXtXLarge" "DenseNet121" "DenseNet169" "DenseNet201" "EfficientNetB0" "EfficientNetB1" "EfficientNetB2" "EfficientNetB3" "EfficientNetB4" "EfficientNetB5" "EfficientNetB6" "EfficientNetB7" "EfficientNetV2B0" "EfficientNetV2B1" "EfficientNetV2B2" "EfficientNetV2B3" "EfficientNetV2L" "EfficientNetV2M" "EfficientNetV2S" "InceptionResNetV2" "InceptionV3" "MobileNet" "MobileNetV2" "NASNetLarge" "NASNetMobile" "ResNet50" "ResNet101" "ResNet152" "ResNet50V2" "ResNet101V2" "ResNet152V2" "VGG16" "VGG19" "Xception"
+for m in "ConvNeXtBase" "ConvNeXtLarge" "ConvNeXtSmall" "ConvNeXtTiny" "ConvNeXtXLarge" "DenseNet121" "DenseNet169" "DenseNet201" "EfficientNetB0" "EfficientNetB1" "EfficientNetB2" "EfficientNetB3" "EfficientNetB4" "EfficientNetB5" "EfficientNetB6" "EfficientNetB7" "EfficientNetV2B0" "EfficientNetV2B1" "EfficientNetV2B2" "EfficientNetV2B3" "EfficientNetV2L" "EfficientNetV2M" "EfficientNetV2S" "InceptionResNetV2" "InceptionV3" "MobileNet" "MobileNetV2" "NASNetLarge" "NASNetMobile" "ResNet50" "ResNet101" "ResNet152" "ResNet50V2" "ResNet101V2" "ResNet152V2" "VGG16" "VGG19" "Xception" "MobileNetV3Large" "MobileNetV3Small"
 do
-    mlflow run --experiment-name=$exp_name -e main.py -P model=$m -P datadir=/data/d1/fischer_diss/imagenet -P subset=$1 ./experiments/imagenet
-    sleep 10
-done
-
-for m in "ConvNeXtBase" "ConvNeXtLarge" "ConvNeXtSmall" "ConvNeXtTiny" "ConvNeXtXLarge" "DenseNet121" "DenseNet169" "DenseNet201" "EfficientNetB0" "EfficientNetB1" "EfficientNetB2" "EfficientNetB3" "EfficientNetB4" "EfficientNetB5" "EfficientNetB6" "EfficientNetB7" "EfficientNetV2B0" "EfficientNetV2B1" "EfficientNetV2B2" "EfficientNetV2B3" "EfficientNetV2L" "EfficientNetV2M" "EfficientNetV2S" "InceptionResNetV2" "InceptionV3" "MobileNet" "MobileNetV2" "NASNetLarge" "NASNetMobile" "ResNet50" "ResNet101" "ResNet152" "ResNet50V2" "ResNet101V2" "ResNet152V2" "VGG16" "VGG19" "Xception"
-do
-    mlflow run --experiment-name=$exp_name -e main.py -P model=$m -P datadir=/data/d1/fischer_diss/imagenet -P subset=$1 -P nogpu=1 ./experiments/imagenet
-    sleep 10
+    for g in "0" "1"
+    do
+        mlflow run --experiment-name=$exp_name -e main.py -P model=$m -P datadir=/data/d1/fischer_diss/imagenet -P subset=$1 -P nogpu=$g ./experiments/imagenet
+        sleep 15
+    done
 done
 
 mlflow experiments csv -x $exp_id > "$exp_name.csv"
