fedb39c5d5698904e84c1b18f7654e1e1436d02e
Raphael Fischer <27725573+raphischer@users.noreply.github.com>
2025-01-11 15:35:47 +0100

    added parse dblp data

--- figures/diss/parse_dblp.py
+++ figures/diss/parse_dblp.py
@@ -4,6 +4,7 @@ import requests
 from time import sleep
 import xml.etree.ElementTree as ET
 
+from tqdm import tqdm
 import pandas as pd
 
 
@@ -39,50 +40,47 @@ def query_papers(keywords, type, max_batches=100, batch_size=1000):
     return results
 
 
-def parse_results(feeds, keywords, logic='arxiv'):
-    print(f"Parsing {logic} results...")
-    keyword_counts = {kw: {} for kw in keywords.values()}
-    all_titles, removed_titles = {}, []
-
-    try:
-        for feed in feeds:
+def parse_results(feeds):
+    data = []
+    for feed in tqdm(feeds):
+        try:
             root = ET.fromstring(feed)
-            if logic == 'arxiv':
-                entries = root.findall("atom:entry", {"atom": "http://www.w3.org/2005/Atom"})
-            else: # dblp:
-                titles = root.findall(".//hit/info/title")
-                years = root.findall(".//hit/info/year")
-                entries = zip(titles, years)
+            titles = root.findall(".//hit/info/title")
+            years = root.findall(".//hit/info/year")
+            authors = root.findall(".//hit/info/authors")
+            entries = zip(titles, years, authors)
             for entry in entries:
-                # extract title
-                if logic == 'arxiv':
-                    title = entry.find("atom:title", {"atom": "http://www.w3.org/2005/Atom"}).text.lower() or ""
-                    year = 'n.a.'
-                else: # dblp:
-                    title = entry[0].text.lower() or ""
-                    year = int(entry[1].text.lower()) or ""
-                added = {}
-                if not (" ai " in title or "intelligen" in title or 'machine learning' in title):
-                    removed_titles.append(title)
-                    continue
-                for keyword, family in keywords.items():
-                    if family not in added:
-                        if title.find(keyword) >= 0:
-                            title = title.replace(keyword, keyword.upper())
-                            added[family] = True
-                            if year not in keyword_counts[family]:
-                                keyword_counts[family][year] = 0
-                            keyword_counts[family][year] += 1
-                if len(added) == 0:
-                    removed_titles.append(title)
-                else:
-                    if year not in all_titles:
-                        all_titles[year] = []
-                    all_titles[year].append(title)
-    except ET.ParseError:
-        print("Error parsing XML feed.")
+                title = entry[0].text.lower()
+                if any([kw in title for kw in [" ai", "ai ", "intelligen", 'machine learning']]):
+                    data.append({
+                        'title': entry[0].text.replace(';', '$$$$$$$$'),
+                        'year': int(entry[1].text) or -1,
+                        'author': ' and ' .join([e.text for e in entry[2].findall("author")]).replace(';', '$$$$$$$$')
+                    })
+        except ET.ParseError:
+            print("Error parsing XML feed.")
+    return pd.DataFrame(data).sort_values(by='year')
+                
 
-    return keyword_counts, all_titles, removed_titles
+
+                # added = {}
+                # if not (" ai " in title or "intelligen" in title or 'machine learning' in title):
+                #     removed_titles.append(title)
+                #     continue
+                # for keyword, family in keywords.items():
+                #     if family not in added:
+                #         if title.find(keyword) >= 0:
+                #             title = title.replace(keyword, keyword.upper())
+                #             added[family] = True
+                #             if year not in keyword_counts[family]:
+                #                 keyword_counts[family][year] = 0
+                #             keyword_counts[family][year] += 1
+                # if len(added) == 0:
+                #     removed_titles.append(title)
+                # else:
+                #     if year not in all_titles:
+                #         all_titles[year] = []
+                #     all_titles[year].append(title)
 
 if __name__ == "__main__":
 
@@ -96,20 +94,19 @@ if __name__ == "__main__":
         "sustain": "Sustainability",
     }
 
-    journals = query_papers(keywords, type='Journal_Articles', max_batches=2)
-    conferences = query_papers(keywords, type='Conference_and_Workshop_Papers', max_batches=2)
-    books = query_papers(keywords, type='Parts_in_Books_or_Collections', max_batches=2)
+    journals = query_papers(keywords, type='Journal_Articles')
+    conferences = query_papers(keywords, type='Conference_and_Workshop_Papers')
+    books = query_papers(keywords, type='Parts_in_Books_or_Collections')
     all_results = journals + conferences + books
-    dblp_counts, dblp_titles, removed_titles = parse_results(all_results, keywords, logic='dblp')
+    data = parse_results(all_results)
+    data.to_csv(os.path.join(os.path.dirname(__file__), "parse_dblp_data.csv"), index=False, sep=';')
+    assert ';' not in data.to_string()
 
-    # print("Keyword occurrences in ArXiv publications:")
-    # for keyword, count in arxiv_counts.items():
-    #     print(f"{keyword}: {count}")
+    # dblp_counts, dblp_titles, removed_titles = parse_results(all_results, keywords, logic='dblp')
 
     # file with counts / summary
-    dblp_df = pd.DataFrame(dblp_counts).sort_index()
-    dblp_df.to_csv(os.path.join(os.path.dirname(__file__), "dblp_counts.csv"))
-
+    # dblp_df = pd.DataFrame(dblp_counts).sort_index()
+    
     # 
-    with open(os.path.join(os.path.dirname(__file__), "dblp_titles.csv"), 'w') as jf:
-        json.dump(dblp_titles, jf, indent=4)
+    # with open(os.path.join(os.path.dirname(__file__), "dblp_titles.csv"), 'w') as jf:
+    #     json.dump(dblp_titles, jf, indent=4)
